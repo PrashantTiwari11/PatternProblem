ac41075937049109046e36c0366c3b15d3d29abd
Prashant <pt867987@gmail.com>
2026-03-12 09:08:42 +0530

    Solve some star patterns.

---
+++ pattern.py
@@ -0,0 +1,17 @@
+n=5;
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for k in range(i):
+        print("*",end=" ")
+    for l in range(i+1):
+        print("*",end=" ")
+    print()
+
+n=5;
+for i in range(n):
+    for j in range(n-i):
+        print(" ",end=' ')
+    for k in range(2*i+1):
+        print("*",end=' ')
+    print()
